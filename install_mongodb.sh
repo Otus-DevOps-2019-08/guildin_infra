54b4accc4ff51263f102d45b450649d40e03c854
guildin <alx.tikhonov@gmail.com>
2019-09-23 17:37:17 +0300

    Travisgit add install_mongodb.sh !

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
+wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list'
 sudo apt update
 sudo apt install -y mongodb-org
