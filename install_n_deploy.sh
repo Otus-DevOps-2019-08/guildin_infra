7e9986a530d77ea8e5e53123e71a343f824ea351
guildin <alx.tikhonov@gmail.com>
2019-09-23 17:19:25 +0300

    Travis, QQ

--- install_n_deploy.sh
+++ install_n_deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo apt update
 sudo apt install -y ruby-full ruby-bundler build-essential
+
 #mongodb install
-#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv E52529D4
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add
 sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list'
 sudo apt update
