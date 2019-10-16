2e36374476f19aac81cd9430b5c8616b467bcf0e
guildin <alx.tikhonov@gmail.com>
2019-10-16 06:25:07 +0300

    TF-2 / app+db
    следующий пункт - модули

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -26,3 +26,11 @@ variable vmcount {
   description = "Number of instances"
   default     = "1"
 }
+variable app_disk_image {
+  description = "Disk image for reddit app"
+  default = "reddit-app-base"
+}
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-db-base"
+}
